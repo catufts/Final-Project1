2a84aa06ab5b3daa1fa79f6aff146b2f34bede02
catufts <corey.a.tufts@gmail.com>
2015-04-27 09:38:26 -0700

    more jquery functions
    Completed all 6 of the query functions!

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
 
-    $("#image3").hover(function(){
+    /*$("#image3").hover(function(){
         $("#image3").css("-webkit-filter", "brightness(50%) contrast(108%)");
         },function(){
         $("#image3").css("-webkit-filter", "brightness(100%) contrast(108%)");
-    });
+    }); */
 
 $("#image2").hover(function(){
         $("#image2").css("-webkit-filter", "brightness(50%) contrast(108%)");
@@ -15,15 +15,31 @@ $("#image2").hover(function(){
 $(window).scroll(function(){
         var height = $(window).scrollTop();
         if (height == 5) {
-        	$("#image2").fadeIn();
+        	$("#image2").fadeIn(1000);
         };
     });
 
     $(window).scroll(function(){
-        var height = $("#image2").fadeIn()
+        var height = $("#image2").fadeIn(1000)
     });
 
+$(".bottom_container").click(function(){
+	$("html, body").animate({ scrollTop: 0},0);
+});
 
+$(".info-container-left").click(function(){
+	$("html, body").animate({ scrollTop: 0},0);
+});
+
+$(".right-bullet-point-explained-container").click(function(){
+	$("html, body").animate({ scrollTop: 0},0);
+});
+
+$(window).scroll(function(){
+  if($(document).scrollTop() > 692){
+    $("#image2").hide();
+  };
+});
 
 
 
